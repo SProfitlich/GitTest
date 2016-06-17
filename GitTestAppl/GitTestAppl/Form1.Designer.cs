71c3278b6cb1e362aeebb478022407167331ae16
SProfitlich <SProfitlich@stuertz.com>
2016-06-17 10:25:52 +0200

    FrmMain - Design done

---
+++ GitTestAppl/GitTestAppl/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace GitTestAppl
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnMessageBox = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnMessageBox
+            // 
+            this.btnMessageBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMessageBox.Location = new System.Drawing.Point(126, 116);
+            this.btnMessageBox.Name = "btnMessageBox";
+            this.btnMessageBox.Size = new System.Drawing.Size(102, 47);
+            this.btnMessageBox.TabIndex = 0;
+            this.btnMessageBox.Text = "MsgBox";
+            this.btnMessageBox.UseVisualStyleBackColor = true;
+            this.btnMessageBox.Click += new System.EventHandler(this.btnMessageBox_Click);
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(353, 296);
+            this.Controls.Add(this.btnMessageBox);
+            this.Name = "FrmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Main";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnMessageBox;
+    }
+}
+
